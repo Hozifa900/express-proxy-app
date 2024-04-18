ec8e190ee4948c811ed51e5106b34ac6a4440490
Hozifa Zeanalabdeen <pundit1d@gmail.com>
2024-04-18 04:09:27 -0500

    add port 3000

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-// Define your proxy routes
+app.post("/", (req, res) => {
+  res.send("done");
+});
+app.get("/", (req, res) => {
+  res.send("get done");
+});
+
+const httpsServer = https.createServer(app);
+
+const PORT = 3000;
+httpsServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Define the proxy routes
 /*const proxyOptions = {
   changeOrigin: true,
 };
@@ -17,25 +31,13 @@ const proxies = [
   { path: "/api/v1/statistics", target: "http://54.90.253.254:3003" },
 ];*/
 
-app.post("/", (req, res) => {
-  res.send("done");
-});
-
-app.get("/", (req, res) => {
-  res.send("get done");
-});
-
 // Create proxy middleware for each route
 // proxies.forEach(({ path, target }) => {
 //   app.use(path, createProxyMiddleware({ ...proxyOptions, target }));
 // });
 
 // Create HTTPS server using the default certificate on port 443
-const httpsServer = https.createServer(app);
+
 // const httpsServer = http.createServer(app);
 
 // Listen on port 443 for HTTPS traffic
-const PORT = process.env.PORT || 443;
-httpsServer.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
