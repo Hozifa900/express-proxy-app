a773e5067a0b371c819f62ac86e902fb410d1661
Hozifa Zeanalabdeen <pundit1d@gmail.com>
2024-04-20 13:21:35 -0500

    forword user IP address

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const API_PAYMENT = "http://54.90.253.254:8888/api/orders";
 
 app.use(morgan("dev"));
 app.use(express.json());
+
 app.use((req, res, next) => {
+  // Retrieve IP address from headers
+  const ipAddress =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  // Set the user's IP address in a custom header
+  req.headers["user-ip-address"] = ipAddress;
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
   res.header(
     "Access-Control-Allow-Headers",
@@ -32,8 +38,12 @@ app.post("/api/v1/orders", async (req, res) => {
 
 app.post("/api/v1/statistics", async (req, res) => {
   try {
+    req.body.ipAddress = "hozifa";
+    console.log("hi", req.body);
+
     const response = await axios.post(`${API_ORDER}/statistics`, req.body);
     data = await response.data;
+    console.log(req.body);
     res.json(data);
   } catch (e) {
     console.log(e);
